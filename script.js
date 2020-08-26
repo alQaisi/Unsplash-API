a6dfda22a12b6e22ce66cf989d7de7035f1c5baa
ahmedhamed1312@gmail.com <ahmedhamed1312@gmail.com>
2020-08-26 18:24:24 +0300

    Adding the main script

---
+++ script.js
@@ -0,0 +1,81 @@
+/*
+--Note--:for anyone who likes to use this code please use your own apikey because I'am using the free one and it limited to 50 requests per hour
+*/
+
+
+const imageContainer=document.getElementById('image-container');
+const loader=document.getElementById('loader');
+let photosArray=[];
+let ready=false;
+let imagesLoaded=0;
+let totalImages=0;
+let count=5;
+const apiKey='WcBrqYWHYfkVrGSIfDp0p2dVo1CZt1kMYbIZlc_Rxs8';
+const apiUrl=`https://api.unsplash.com/photos/random?client_id=${apiKey}&count=${count}&content_filter=high`;
+
+const setAttributes=(element,attributes)=>{
+    for(const attribute in attributes){
+        element.setAttribute(attribute,attributes[attribute]);
+    }
+}
+const imageLoaded=()=>{
+    imagesLoaded++;
+    if(imagesLoaded===totalImages ){
+        count=15;
+        loader.hidden=true;
+        ready=true;
+    }
+}
+
+const displayPhotos=()=>{
+    imagesLoaded=0;
+    totalImages=photosArray.length;
+    photosArray.forEach(photo=>{
+        const item=document.createElement('a');
+        const img=document.createElement('img');
+        setAttributes(item,{
+            'href':photo.links.html,
+            'target':'_blank'
+        });
+        setAttributes(img,{
+            src:photo.urls.regular,
+            alt:photo.alt_description,
+            title:photo.alt_description
+        });
+        img.addEventListener('load',imageLoaded);
+        item.appendChild(img);
+        imageContainer.appendChild(item);
+    })
+}
+
+const fetchPhotos=async ()=>{
+    /*
+    Important Note:
+        the unsplash api key must be hidden so I build a server and I put the unsplash API into it,and by this I can get the response and in sametime have my api key safe
+    */
+    fetch('https://hidden-citadel-03585.herokuapp.com/fetchImages',{
+          method:'post',
+          headers:{'Content-Type':'application/json'},
+          body:JSON.stringify({
+              count:count})
+          }).then(response=>response.json())
+            .then(data=>{
+                photosArray=data;
+                displayPhotos();
+            });
+    // try{
+    //     const response=await fetch(apiUrl);
+    //     photosArray=await response.json();
+    //     displayPhotos();
+    // }catch(err){
+        
+    // }
+}
+
+window.addEventListener('scroll',()=>{
+    if(window.innerHeight+window.scrollY>=document.body.offsetHeight-1000 && ready===true){
+        ready=false;
+        fetchPhotos();
+    }
+})
+fetchPhotos();
